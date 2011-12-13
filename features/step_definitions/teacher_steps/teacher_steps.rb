46be126b94dba52d682d5804fc72dc293c17b929
Geoffrey Kwan <geoffreykwan@gmail.com>
2011-12-13 14:45:43 -0800

    Added more Authoring Tool tests.

--- features/step_definitions/teacher_steps/teacher_steps.rb
+++ features/step_definitions/teacher_steps/teacher_steps.rb
@@ -1,17 +1,22 @@
 
-
-#Given
+#############
+### Given ###
+#############
 
 Given /^I am logged in as a teacher$/ do
   login($teacherLogin, $teacherPassword);
   page.should have_content("Teacher Home")
 end
 
-#When
+#############
+### When ####
+#############
 
-When /^I click on the "([^"]*)" button in the Authoring Tool$/ do |buttonText|
+When /^I click on the "([^"]*)" button in the Authoring Tool$/ do |text|
   within_frame("authorfrm") do
-    find_by_id(buttonText).click
+    #capybara can't handle confirm dialog popups so we will just override the confirm function to always return true so we can move on with the test
+    page.evaluate_script('window.confirm = function() { return true; }')
+    click_button(text)
   end
 end
 
@@ -27,6 +32,7 @@ When /^I click on the "([^"]*)" link and say OK to the warning$/ do |link|
   find('a', :text => link).click
 end
 
+#click on the grading link
 When /^I click on the "([^"]*)" "([^"]*)" Grading Tool link$/ do |gradeBy, revisions|
   gradingType = "";
   getRevisions = "";
@@ -89,8 +95,11 @@ When /^period "([^"]*)" is selected$/ do |period|
       if period == "All Periods"
         periodId = "all"
       else
+        #make everything lowercase and remove all spaces
         periodId = period.downcase.gsub(/ /, '')
       end
+      
+      #make sure the period is selected
       page.should have_css('#' + periodId + '.checked')
     end
   end
@@ -102,6 +111,7 @@ When /^I click on period "([^"]*)"$/ do |period|
       if period == "All Periods"
         periodId = "all"
       else
+        #make everything lowercase and remove all spaces
         periodId = period.downcase.gsub(/ /, '')
       end
       find_by_id(periodId).click
@@ -109,10 +119,10 @@ When /^I click on period "([^"]*)"$/ do |period|
   end
 end
 
+#click on the given step in the grading tool
 When /^I click on Step "([^"]*)" in the Grading Tool$/ do |stepPosition|
   within_frame("gradingIfrm") do
     within_frame("topifrm") do
-      #click_link(stepPosition)
       find('a', :text => stepPosition).click
     end
   end
@@ -122,9 +132,12 @@ end
 When /^I give a student a score of "([^"]*)" in the Grading Tool$/ do |score|
   within_frame("gradingIfrm") do
     within_frame("topifrm") do
+      #get all the text input elements
       elements = page.all('input', :type => 'text');
       
+      #loop through all the elements
       elements.each { |element|
+        #check that the type is text and the element is enabled
       	if element[:type] == 'text' and element[:disabled] == 'false'
       	  element.set(score)
       	  break
@@ -140,6 +153,7 @@ When /^I give a student a comment of "([^"]*)" in the Grading Tool$/ do |comment
       elements = page.all('textarea');
       
       elements.each { |element|
+        #check that the element is enabled
       	if element[:disabled] == 'false'
       	  element.set(comment)
       	  break
@@ -169,7 +183,198 @@ When /^I click on the student "([^"]*)" in the Grading Tool$/ do |studentLogin|
   end
 end
 
-#Then
+When /^I see "([^"]*)" in the Authoring Tool$/ do |text|
+  within_frame("authorfrm") do 
+    page.should have_content(text)
+  end
+end
+
+When /^I see the "([^"]*)" popup in the Authoring Tool$/ do |elementId|
+  within_frame("authorfrm") do 
+    find('#' + elementId).visible?.should == true
+  end
+end
+
+When /^I should not see the "([^"]*)" popup in the Authoring Tool$/ do |elementId|
+  within_frame("authorfrm") do 
+    find('#' + elementId).visible?.should == false
+  end
+end
+
+When /^I enter "([^"]*)" into the "([^"]*)" field in the Authoring Tool$/ do |text, elementId|
+  within_frame("authorfrm") do 
+    find_by_id(elementId).set(text)
+  end
+end
+
+When /^I enter a unique value starting with "([^"]*)" into the "([^"]*)" field in the Authoring Tool$/ do |text, elementId|
+  within_frame("authorfrm") do 
+    #get the current time
+    time = Time.now
+    
+    #get the time in milliseconds
+    text += " " + time.to_i.to_s
+    
+    #remember the input value so we can refer to it in future steps
+    $tempInputValue = text
+    
+    find_by_id(elementId).set(text)
+  end
+end
+
+#select an option from a select box
+#optionValue can be the text value of an option or the index
+#of the option in the select box or "first" or "last" for
+#the respective first or last elements in the select box
+When /^I select the element "([^"]*)" in group name "([^"]*)" in the "([^"]*)" select box in the Authoring Tool$/ do |optionValue, groupName, elementId|
+  within_frame("authorfrm") do 
+
+    #get all the option elements
+    options = page.all('option')
+    
+    optionToSelect = nil
+    
+    #loop through all the option elements
+    options.each { |option|
+      #find the option elements for the select box
+      if option[:name] == groupName
+        #check if we have found the option with the value we want
+        if option.value == optionValue || option.text == optionValue
+          #we have found the value so we will break out of the loop
+          optionToSelect = option
+          break
+        end
+      end
+    }
+    
+    if optionToSelect == nil
+      #we did not find the option by value so we will try to find it by index or first or last
+      
+      counter = 0
+      
+      #loop through all the option elements
+      options.each { |option|
+        #find the option elements for the select box
+        if option[:name] == groupName
+
+          if optionValue == 'first'
+            #we want the first option
+            optionToSelect = option
+            break;
+          elsif optionValue == 'last'
+            #we want the last option
+            optionToSelect = option
+          elsif optionValue.to_i.to_s == optionValue
+            #we want the option at the specific index
+            if optionValue.to_i == counter
+              optionToSelect = option
+              break;
+            end
+          end
+          
+          counter += 1
+        end
+      }    
+    end
+    
+    #puts optionToSelect.text
+    page.select optionToSelect.text, :from => elementId
+  end
+end
+
+When /^I click on the element with id "([^"]*)" in the Authoring Tool$/ do |elementId|
+  within_frame("authorfrm") do 
+    find("#" + elementId).click
+  end
+end
+
+When /^I click on the "([^"]*)" activity in the Authoring Tool$/ do |arg1|
+  within_frame("authorfrm") do
+    puts find('.master')[:id]
+    find('.master').click
+  end
+end
+
+When /^I click on the master node in the Authoring Tool$/ do
+  within_frame("authorfrm") do
+    find('.master').click
+  end
+end
+
+When /^I click on the first activity in the Authoring Tool$/ do
+  within_frame("authorfrm") do
+    find('.seq').click
+  end
+end
+
+When /^I select the first activity in the Authoring Tool$/ do
+  within_frame("authorfrm") do
+    #get the first activity elements
+    seqDiv = find('.seq')
+    
+    #get the checkbox
+    selectCheckBox = seqDiv.all('input')[0]
+    selectCheckBox.click
+    
+    #get the title of the activity
+    titleInput = seqDiv.all('input')[1]
+    $tempInputValue = titleInput.value
+    
+    #get the number of activities
+    activities = page.all('.seq')
+    $tempActivityCount = activities.length
+  end
+end
+
+When /^I click on the second activity in the Authoring Tool$/ do
+  within_frame("authorfrm") do
+    seqDivs = page.all('.seq')
+    seqDiv = seqDivs[1]
+    seqDiv.click
+  end
+end
+
+When /^I select the first step in the Authoring Tool$/ do
+  within_frame("authorfrm") do
+    #get the step
+    stepDiv = find('.node')
+    
+    #get thecheckbox
+    selectCheckBox = stepDiv.all('input')[0]
+    selectCheckBox.click
+    
+    #get the title of the step
+    titleInput = stepDiv.all('input')[1]
+    $tempInputValue = titleInput.value
+    
+    #get the number of steps
+    steps = page.all('.node')
+    $tempStepCount = steps.length
+  end
+end
+
+When /^I click on the second step in the Authoring Tool$/ do
+  within_frame("authorfrm") do
+    #get all the steps
+    stepDivs = page.all('.node')
+    
+    #get the seconds step
+    stepDiv = stepDivs[1]
+    stepDiv.click
+  end
+end
+
+When /^I click edit on the first step in the Authoring Tool$/ do
+  within_frame("authorfrm") do
+    #get the first Edit button
+    editButton = find('.editNodeInput')
+    editButton.click
+  end
+end
+
+#############
+### Then ####
+#############
 
 Then /^I should see "([^"]*)" in the Authoring Tool$/ do |text|
   within_frame("authorfrm") do 
@@ -184,6 +389,7 @@ Then /^I should see project "([^"]*)" loaded in the Authoring Tool$/ do |project
 end
 
 Then /^I should see the preview window open$/ do
+  #check within the new window
   within_window(page.driver.browser.window_handles.last) do
     within_frame("topifrm") do 
       page.should have_selector('#contentDiv')
@@ -219,8 +425,11 @@ Then /^period "([^"]*)" should be selected$/ do |period|
       if period == "All Periods"
         periodId = "all"
       else
+        #make everything lowercase and remove all spaces
         periodId = period.downcase.gsub(/ /, '')
       end
+      
+      #make sure the period is selected
       page.should have_css('#' + periodId + '.checked')
     end
   end
@@ -230,10 +439,13 @@ end
 Then /^I should see a student with a score of "([^"]*)" in the Grading Tool$/ do |score|
   within_frame("gradingIfrm") do
     within_frame("topifrm") do
+      #get all the text inputs
       elements = page.all('input', :type => 'text');
       
       elements.each { |element|
+        #make sure the element is type text and is enabled
       	if element[:type] == 'text' and element[:disabled] == 'false'
+      	  #compare the score
       	  element.value.should == score
       	  break
       	end
@@ -246,10 +458,13 @@ end
 Then /^I should see a student with a comment of "([^"]*)" in the Grading Tool$/ do |comment|
   within_frame("gradingIfrm") do
     within_frame("topifrm") do
+      #get all the textarea elemens
       elements = page.all('textarea');
       
       elements.each { |element|
+        #make sure the element is enabled
       	if element[:disabled] == 'false'
+      	  #compare the comment
       	  element.value.should == comment
       	  break
       	end
@@ -259,7 +474,125 @@ Then /^I should see a student with a comment of "([^"]*)" in the Grading Tool$/ 
 end
 
 Then /^I should see the Premade Comments window open$/ do
+  #check within the new window
   within_window(page.driver.browser.window_handles.last) do
     page.should have_content("Global Premade Comment List")
+  end
+end
+
+Then /^I should see "([^"]*)" in the "([^"]*)" field in the Authoring Tool$/ do |text, elementId|
+  within_frame("authorfrm") do 
+    find_by_id(elementId).set(text)
+  end
+end
+
+Then /^I should see the new activity as the first activity in the Authoring Tool$/ do
+  within_frame("authorfrm") do 
+    #get the id of the first activity
+    seqDivId = find('.seq')[:id]
+    
+    #get the activity number
+    seqNum = /master--seq_(\d*)--0/.match(seqDivId)[1]
+    
+    #get the id of the title input
+    titleInputId = 'titleInput_seq_' + seqNum
+    
+    #compare the activity title
+    find('#' + titleInputId).value.should == $tempInputValue
+  end
+end
+
+Then /^I should see the new step in the first activity in the Authoring Tool$/ do
+  within_frame("authorfrm") do 
+    #get the first step
+    firstStep = find('.node')
+    nodeDivId = firstStep[:id]
+    
+    #get the node id
+    nodeId = /.*--(.*)--.*/.match(nodeDivId)[1]
+    
+    #get the title input
+    titleInput = firstStep.all('input')[1]
+    titleInput.value.should == $tempInputValue
+  end
+end
+
+Then /^I should see the activity as the second activity in the Authoring Tool$/ do
+  within_frame("authorfrm") do 
+    #get the title of the activity previously manipulated
+    activityTitle = $tempInputValue
+    
+    #get all the activities
+    seqDivs = page.all('.seq')
+    
+    #get the seconds activity
+    seqDiv = seqDivs[1]
+    
+    #get the title
+    titleInput = seqDiv.all('input')[1]
+    
+    #compare the title with our saved activity title
+    titleInput.value.should == activityTitle
+  end
+end
+
+Then /^I should see the step as the second step in the Authoring Tool$/ do
+  within_frame("authorfrm") do 
+    #get the step title that we previously manipulated
+    stepTitle = $tempInputValue
+    
+    #get all the steps
+    stepDivs = page.all('.node')
+    
+    #get the second step
+    stepDiv = stepDivs[1]
+    
+    #get the title
+    titleInput = stepDiv.all('input')[1]
+    
+    #compare the title with our saved step title
+    titleInput.value.should == stepTitle
+  end
+end
+
+Then /^I should see the number of steps increase in the Authoring Tool$/ do
+  within_frame("authorfrm") do 
+    steps = page.all('.node')
+    steps.length.should == ($tempStepCount + 1)
+  end
+end
+
+Then /^I should see the number of activities increase in the Authoring Tool$/ do
+  within_frame("authorfrm") do 
+    activities = page.all('.seq')
+    activities.length.should == ($tempActivityCount + 1)
+  end
+end
+
+Then /^I should see the number of steps decrease in the Authoring Tool$/ do
+  within_frame("authorfrm") do 
+    steps = page.all('.node')
+    steps.length.should == ($tempStepCount - 1)
+  end
+end
+
+Then /^I should see the number of activities decrease in the Authoring Tool$/ do
+  within_frame("authorfrm") do 
+    activities = page.all('.seq')
+    activities.length.should == ($tempActivityCount - 1)
+  end
+end
+
+Then /^I should see the previously saved content in the "([^"]*)" field in the Authoring Tool$/ do |elementId|
+  within_frame("authorfrm") do
+    find_by_id(elementId).value.should == $tempInputValue
+  end
+end
+
+Then /^I should see the previously saved content in the preview frame in the Authoring Tool$/ do
+  within_frame("authorfrm") do
+    within_frame("previewFrame") do
+      should have_content($tempInputValue);
+    end
   end
 end
---
+++ features/teacher_features/authoring_tool_features/authoring_tool_page.feature
@@ -0,0 +1,197 @@
+Feature: Author a project in the Authoring Tool
+  In order to author a project,
+  As a teacher
+  I want to be able to perform authoring in the Authoring Tool
+  
+  Scenario: Launch authoring tool and open a project
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    Then I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    
+  Scenario: Create a project
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I see "Exit to Home" in the Authoring Tool
+    And I click on the "Create Project" button in the Authoring Tool
+    And I see the "createProjectDialog" popup in the Authoring Tool
+    And I enter "Test Project" into the "projectInput" field in the Authoring Tool
+    And I click on the "Submit" button in the Authoring Tool
+    Then I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I should see "Test Project" in the "projectTitleInput" field in the Authoring Tool
+    
+  Scenario: Copy a project
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I see "Exit to Home" in the Authoring Tool
+    And I click on the "Copy Project" button in the Authoring Tool
+    And I see the "copyProjectDialog" popup in the Authoring Tool
+    And I select the element "last" in group name "copyProjectOption" in the "copyProjectSelect" select box in the Authoring Tool
+    And I click on the "Copy" button in the Authoring Tool
+    Then I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    
+  Scenario: Add an activity to a project
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I click on the "Add Activity" button in the Authoring Tool
+    And I see the "createSequenceDialog" popup in the Authoring Tool
+    And I enter a unique value starting with "Activity Test" into the "createSequenceInput" field in the Authoring Tool
+    And I click on the "Submit" button in the Authoring Tool
+    And I click on the master node in the Authoring Tool
+    Then I should see the new activity as the first activity in the Authoring Tool
+  
+  Scenario: Add a step to a project
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I click on the "Add Step" button in the Authoring Tool
+    And I see the "createNodeDialog" popup in the Authoring Tool
+    And I enter a unique value starting with "Step Test" into the "createNodeTitle" field in the Authoring Tool
+    And I select the element "Open Response" in group name "selectStepTypeOption" in the "createNodeType" select box in the Authoring Tool
+    And I click on the "Submit" button in the Authoring Tool
+    And I click on the first activity in the Authoring Tool
+    Then I should see the new step in the first activity in the Authoring Tool
+    
+  Scenario: Move an activity in a project
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I select the first activity in the Authoring Tool
+    And I click on the "Move" button in the Authoring Tool
+    And I click on the second activity in the Authoring Tool
+    Then I should see the activity as the second activity in the Authoring Tool
+
+  Scenario: Move a step in a project
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I select the first step in the Authoring Tool
+    And I click on the "Move" button in the Authoring Tool
+    And I click on the second step in the Authoring Tool
+    Then I should see the step as the second step in the Authoring Tool
+
+  Scenario: Duplicate a step in a project
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I select the first step in the Authoring Tool
+    And I click on the "Duplicate" button in the Authoring Tool
+    Then I should see the number of steps increase in the Authoring Tool
+    
+  Scenario: Duplicate an activity in a project
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I select the first activity in the Authoring Tool
+    And I click on the "Duplicate" button in the Authoring Tool
+    Then I should see the number of activities increase in the Authoring Tool
+    
+  Scenario: Delete a step in a project
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I select the first step in the Authoring Tool
+    And I click on the "Delete" button in the Authoring Tool
+    Then I should see the number of steps decrease in the Authoring Tool
+
+  Scenario: Delete an activity in a project
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I select the first activity in the Authoring Tool
+    And I click on the "Delete" button in the Authoring Tool
+    Then I should see the number of activities decrease in the Authoring Tool
+    
+  Scenario: Edit a step in a project and make sure it saves
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I click edit on the first step in the Authoring Tool
+    And I see the "authorStepDialog" popup in the Authoring Tool
+    And I enter a unique value starting with "Hello" into the "promptInput" field in the Authoring Tool
+    And I click on the "Save & Close" button in the Authoring Tool
+    And I should not see the "authorStepDialog" popup in the Authoring Tool
+    And I click edit on the first step in the Authoring Tool
+    Then I should see the previously saved content in the "promptInput" field in the Authoring Tool
+    
+  Scenario: Edit a step in a project and make sure the preview frame updates
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I click edit on the first step in the Authoring Tool
+    And I see the "authorStepDialog" popup in the Authoring Tool
+    And I enter a unique value starting with "Hello" into the "promptInput" field in the Authoring Tool
+    And I click on the "Save & Close" button in the Authoring Tool
+    And I should not see the "authorStepDialog" popup in the Authoring Tool
+    And I click edit on the first step in the Authoring Tool
+    Then I should see the previously saved content in the preview frame in the Authoring Tool
+    
+  Scenario: Preview a project in the Authoring Tool
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I click on the "Preview Project" button in the Authoring Tool
+    Then I should see the preview window open
+    
+  Scenario: Exit to Home in the Authoring Tool
+    Given I am logged in as a teacher
+    When I click on the "Management" link
+    And I click on the "Launch Authoring Tool" link
+    And I click on the "Open Project" button in the Authoring Tool
+    And I select the element "last" in group name "projectOption" in the "selectProject" select box in the Authoring Tool
+    And I should see "Project Title" in the Authoring Tool
+    And I should see "ID" in the Authoring Tool
+    And I click on the "Exit to Home" button in the Authoring Tool
+    Then I should see "Teacher Home"
